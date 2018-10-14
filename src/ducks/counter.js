37994de341004e77e1ab297a2b460ef60298cbb6
Ethan Sanders <ethanedu5@gmail.com>
2018-10-14 17:08:59 -0500

    Finished mini

---
+++ src/ducks/counter.js
@@ -0,0 +1,54 @@
+const initialState = {
+    currentValue:0,
+    futureValues: [],
+    previousValues:[]
+};
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const UNDO = "UNDO";
+const REDO = "REDO";
+
+export default function reducer(state = initialState,action){
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                currentValue:state.currentValue+action.amount,
+                futureValues:[],
+                previousValues:[state.currentValue,...state.previousValues]
+            };
+            case DECREMENT:
+            return {
+                currentValue:state.currentValue-action.amount,
+                futureValues:[],
+                previousValues:[state.currentValue,...state.previousValues]
+            };
+        case UNDO:
+            return {
+                currentValue:state.previousValues[0],
+                futureValues:[state.currentValue,...state.futureValues],
+                previousValues:state.previousValues.slice(1)
+            };
+        case REDO:
+            return {
+                currentValue:state.futureValues[0],
+                futureValues:state.futureValues.slice(1),
+                previousValues:[state.currentValue,...state.previousValues]
+            };
+        default:
+            return state;
+    }
+}
+
+export function increment(amount){
+    return {type:INCREMENT,amount}
+}
+export function decrement(amount){
+    return {type:DECREMENT,amount}
+}
+export function redo(){
+    return {type:REDO}
+}
+export function undo(){
+    return {type:UNDO}
+}
